cddddd612a81bfe745a35793541460edecc2f3b8
Daniel Solano Gómez <daniel@solanogomez.org>
2015-04-15 12:24:06 -0500

    Add GUI for tetris

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main ^:skip-aot tetris.core
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [seesaw "1.4.5"]]
+  :main ^:skip-aot tetris.gui
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
---
+++ src/tetris/gui.clj
@@ -0,0 +1,110 @@
+(ns tetris.gui
+  (:require [seesaw.border :as border]
+            [seesaw.core :as seesaw]
+            [seesaw.graphics :as graphics]
+            [seesaw.keymap :as keymap]
+            [tetris.core :as tetris])
+  (:gen-class))
+
+(defn ^:private make-ui
+  "Creates a new frame for the Tetris game."
+  []
+  (seesaw/frame
+    :title "Tetris"
+    :resizable? false
+    :on-close :dispose
+    :content (seesaw/border-panel
+               :border 5
+               :hgap 5
+               :vgap 5
+               :north (seesaw/button
+                        :id :new-game
+                        :text "New Game"
+                        :mnemonic \n)
+               :center (seesaw/canvas
+                         :id :canvas
+                         :background :black
+                         :size [200 :by 400]
+                         :border (border/line-border
+                                   :thickness 2
+                                   :color :black)))))
+
+(defn ^:private make-block-renderer
+  "Returns a function that will render a block at the given coordinates and
+  with the given colour."
+  [canvas graphics board]
+  (let [rows (count board)
+        cols (count (first board))
+        cellh (/ (seesaw/height canvas) rows)
+        cellw (/ (seesaw/width canvas) cols)
+        rx (* 0.25 cellw)
+        ry (* 0.25 cellh)]
+    (fn [x y colour]
+      (graphics/draw graphics
+                     (graphics/rounded-rect (inc (* x cellw)) (inc (* y cellh))
+                                            (- cellw 2) (- cellh 2)
+                                            rx ry)
+                     (graphics/style :background colour)))))
+
+(defn ^:private render-board!
+  "Draws the given game state to the canvas using the graphics context."
+  [render-block board]
+  (let [rows (count board)
+        cols (count (first board))]
+    (doseq [x (range cols)
+            y (range rows)]
+      (when-not (zero? (get-in board [y x]))
+        (render-block x y :grey)))))
+
+(defn ^:private render-tetrimino!
+  "Draws the tetrimino at the given position"
+  [render-block tetrimino position]
+  (dorun (map (fn [offset]
+                (let [[x y] (map + position offset)]
+                  (render-block x y :yellow)))
+              tetrimino)))
+
+(defn ^:private render-game!
+  "Draws the given game state to the canvas using the graphics context."
+  [canvas graphics {:keys [board tetrimino position]}]
+  (let [render-block (make-block-renderer canvas graphics board)]
+    (render-board! render-block board)
+    (render-tetrimino! render-block tetrimino position)))
+
+(defn ^:private add-behaviours
+  "Takes a game user interface and adds the dynamic behaviours for the game.
+  Returns the UI."
+  [root game-atom]
+  (seesaw/listen (seesaw/select root [:#new-game])
+                 :action  (fn [_] (reset! game-atom (tetris/new-game))))
+  (doto root
+    (keymap/map-key "W" (fn [_] (println "rotate!")))
+    (keymap/map-key "A" (fn [_] (swap! game-atom update-in [:position 0] dec)))
+    (keymap/map-key "S" (fn [_] (swap! game-atom update-in [:position 1] inc)))
+    (keymap/map-key "D" (fn [_] (swap! game-atom update-in [:position 0] inc)))
+    (seesaw/listen :window-closing (fn [_] (remove-watch game-atom :gui))))
+  (let [canvas (seesaw/select root [:#canvas])]
+    (seesaw/config! canvas
+                    :paint (fn [canvas graphics] (render-game! canvas graphics @game-atom)))
+    (add-watch game-atom :gui (fn [_ _ _ _] (seesaw/repaint! canvas))))
+  root)
+
+(defn create-gui!
+  "Creates a new user interface that will respond to the given game reference."
+  [game-atom]
+  (-> (make-ui)
+      (add-behaviours game-atom)
+      (seesaw/pack!) 
+      (seesaw/show!)))
+
+(comment
+  (require ['seesaw.dev :refer :all])
+  (show-events (seesaw/frame)))
+
+(defn -main
+  "Main entry point for the application."
+  [& args]
+  (let [game (atom (tetris/new-game))
+        gui (create-gui! game)]
+    (seesaw/listen gui
+      :window-closed (fn [_] (System/exit 0)))))
